b4c11fe4e0b00003ddc498daa034c3b5aaeaa7a6
Gunawan Nur Ahmad <54525132+GunawanAhmad@users.noreply.github.com>
2021-11-23 11:25:06 +0700

    add extra tab section

---
+++ src/js/stylePage/extraPageSection.js
@@ -0,0 +1,7 @@
+export function extraPageSection() {
+  $(document).ready(function () {
+    $(".select2").select2({
+      tags: true,
+    });
+  });
+}
--- src/js/stylePage/main.js
+++ src/js/stylePage/main.js
@@ -1,3 +1,5 @@
 import { profileSection } from "./profileSection.js";
+import { extraPageSection } from "./extraPageSection.js";
 
 profileSection();
+extraPageSection();
