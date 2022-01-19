c417a51b18db28d82ff6be7db66ab7b9f16748c5
Gunawan Nur Ahmad <54525132+GunawanAhmad@users.noreply.github.com>
2022-01-19 17:21:22 +0700

    fixing preview

--- src/js/globalHandler/previewHandler.js
+++ src/js/globalHandler/previewHandler.js
@@ -1,14 +1,21 @@
 let previewBtn = document.querySelector(".preview-btn");
 let phoneWrapper = document.querySelector(".phone-wrapper");
 let closePreviewBtn = document.querySelector(".close-preview-btn");
+let mainContentWrapper = document.querySelector('.content-wrapper')
+let header = document.querySelector('header')
+let footer = document.querySelector('footer')
 export function previewHandler() {
   function togglePreview() {
     phoneWrapper.classList.toggle("show");
+    mainContentWrapper.classList.toggle("hide");
+    header.classList.toggle("hide")
+    footer.classList.toggle("hide")
   }
 
   phoneWrapper.addEventListener("click", function (element) {
     if (element.target == this) togglePreview();
   });
+  
   previewBtn.addEventListener("click", togglePreview);
   closePreviewBtn.addEventListener("click", togglePreview);
 }
